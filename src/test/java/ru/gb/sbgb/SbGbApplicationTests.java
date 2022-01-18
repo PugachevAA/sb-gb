64bea1cf8ccda5f2bfd3208541fde87bd1d7684f
Alexey <pugachev.aa@mail.ru>
2022-01-18 22:19:26 +0700

    Initial commit

---
+++ src/test/java/ru/gb/sbgb/SbGbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.sbgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
